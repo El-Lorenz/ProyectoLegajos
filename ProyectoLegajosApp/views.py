946d3cb17a48b512c4cdff4002c560a664965b6f
El-Lorenz <105938619+El-Lorenz@users.noreply.github.com>
2023-10-23 12:48:26 -0300

    actualizacion 23-10

--- ProyectoLegajosApp/views.py
+++ ProyectoLegajosApp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home (request):
+    return HttpResponse("Home")
+
+def blog (request):
+    return HttpResponse("Blog")
+
+def contacto (request):
+    return HttpResponse("Contacto")
