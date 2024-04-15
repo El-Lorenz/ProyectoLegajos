240442e21393fea71a07a8c75ddfdfb0abd3ab7e
El-Lorenz <105938619+El-Lorenz@users.noreply.github.com>
2024-04-15 20:59:08 -0300

    act 15-04

--- ProyectoLegajos/settings.py
+++ ProyectoLegajos/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ProyectoLegajosApp',
+    'autenticacion',
 ]
 
 MIDDLEWARE = [
--- ProyectoLegajos/urls.py
+++ ProyectoLegajos/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('autenticacion/',include('autenticacion.urls')),
     path('', include('ProyectoLegajosApp.urls')),
 
 ]
