2a7089418cdb2778b4e83e35e99e08ed93b92775
El-Lorenz <105938619+El-Lorenz@users.noreply.github.com>
2024-04-17 14:21:04 -0300

    act 17-04

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,7 +1,18 @@
+from typing import Any
 from django.shortcuts import render
+
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
 
 def autenticacion (request):
     
     return render(request, "registro/base_login.html")
+
+# Algorisoft.
+class   LoginFormView(LoginView):
+    template_name='base_login.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
