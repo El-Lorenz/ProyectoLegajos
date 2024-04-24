a2ee1a6575fb4bb23b8d0b6809ac62dc2d256309
El-Lorenz <105938619+El-Lorenz@users.noreply.github.com>
2024-04-24 14:32:48 -0300

    act 24-04
    actualizacion

--- ProyectoLegajos/settings.py
+++ ProyectoLegajos/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ProyectoLegajosApp',
     'autenticacion',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
 TIME_ZONE = 'UTC'
 
@@ -123,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+
--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import View
+
+from django.contrib.auth.forms import UserCreationForm
 
 # Algorisoft.
 #from django.contrib.auth.views import LoginView
 
 # Create your views here.
 
-def autenticacion (request):
+#def autenticacion (request):
     
 #    return render(request, "registro/base_login.html")
-    return render(request, "registro/registro.html")
+
+#    return render(request, "registro/registro.html")
+
+class VRegistro(View):
+    def get(self,request):
+        form=UserCreationForm()
+        return render (request,"registro/registro.html",{"form":form} )
+
+
+    def post(self, request):
+        pass
+
+
 
 
 # Algorisoft.
