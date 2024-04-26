b9a9623d5fb3e83ac9aa85d1638caad8062024d0
El-Lorenz <105938619+El-Lorenz@users.noreply.github.com>
2024-04-26 12:02:08 -0300

    act 26-04
    actualizacion

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import View
 
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login,logout
+from django.contrib import messages
 
 # Algorisoft.
 #from django.contrib.auth.views import LoginView
@@ -21,9 +23,20 @@ class VRegistro(View):
 
 
     def post(self, request):
-        pass
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            usuario=form.save()
+            login(request, usuario)
+            return redirect('Home')
+        else:
+            for msg in form.error_messages:
+                messages.error(request, form.error_messages[msg])
+            return render (request,"registro/registro.html",{"form":form} )
 
 
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('Home')
 
 
 # Algorisoft.
