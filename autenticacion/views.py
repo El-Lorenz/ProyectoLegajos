a2c57f82ee65e8e2469924d138176fc3a5047c67
El-Lorenz <105938619+El-Lorenz@users.noreply.github.com>
2024-04-30 14:13:40 -0300

    30-04
    act

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,redirect
 from django.views.generic import View
 
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login,logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login,logout,authenticate
 from django.contrib import messages
 
 # Algorisoft.
@@ -37,6 +37,25 @@ class VRegistro(View):
 def cerrar_sesion(request):
     logout(request)
     return redirect('Home')
+
+def logear(request):
+    if request.method=="POST":
+        form=AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            nombre_usuario=form.cleaned_data.get("username")
+            contra=form.cleaned_data.get("password")
+            usuario=authenticate(username=nombre_usuario, password=contra)
+            if usuario is not None:
+                login(request,usuario)
+                return redirect('Home')
+            else:
+                messages.error(request,"usuario no valido")
+        else:
+            messages.error(request,"usuario no valido")
+
+
+    form=AuthenticationForm()
+    return render (request,"login/login.html",{"form":form} )
 
 
 # Algorisoft.
